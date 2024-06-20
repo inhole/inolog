1efd3587a19150b30393efd4fa68c2479e695d2a
Ino <sylee74133@gmail.com>
2024-06-20 15:06:50 +0900

    init
    (cherry picked from commit eb878f830ba8bfbfd0e528d26a4977964dd7c769)

---
+++ src/main/java/com/inolog/InologApplication.java
@@ -0,0 +1,13 @@
+package com.inolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InologApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inolog/InologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InologApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
