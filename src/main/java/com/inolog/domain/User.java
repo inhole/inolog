c92ec63bcf60f5f0e169a9a5367b56fdc01f637e
Ino <sylee74133@gmail.com>
2024-07-03 15:24:14 +0900

    User Entity 추가

---
+++ src/main/java/com/inolog/domain/User.java
@@ -0,0 +1,27 @@
+package com.inolog.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate createdAt;
+}
---
+++ src/main/java/com/inolog/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.inolog.repository;
+
+import com.inolog.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
