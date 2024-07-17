cc51cc04b3491b8037345c3089e2a056fbc7f005
Ino <sylee74133@gmail.com>
2024-07-18 01:12:39 +0900

    초기 데이터 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `user` (name, email, password, created_at) VALUES ('ino', 'dev', '$40801$uaoNYQWF+DbwHRIEZmNTOEFnJKyB5itRgeMed+nnZPG0C3842F5ywBkocYXYqKTPFMd5EG7f8zp0I8B5LqECkA==$1dR5xPuZ4NSUO9NV6/e5rTJTR7hYHs2AVIQLfbsmEQg=', '2024-07-03');
+
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
+INSERT INTO POST (title, content, reg_Date, user_Id) VALUES ('별 헤는 밤', '계절이 지나가는 하늘에는', now(), 1);
